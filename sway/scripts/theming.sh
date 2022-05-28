c46b2693b9bb152a55232e49b3f80e9814e66acf
DiegoBarrosA <diegobarrosaraya@outlook.com>
2022-05-28 15:30:08 -0400

    EXPERIMENTAL GNOME MIGRATION

--- sway/scripts/theming.sh
+++
@@ -1,8 +0,0 @@
-#/bin/sh
-nix-shell -p glib --run "
-    gsettings set org.gnome.desktop.interface gtk-theme 'Materia-dark'
-    gsettings set org.gnome.desktop.interface icon-theme 'Papirus-Dark'                                            
-    gsettings set org.gnome.desktop.interface cursor-theme 'capitaine-cursors'
-    gsettings set org.gnome.desktop.interface font-name 'Source Sans 13'
-    gsettings set org.gnome.desktop.interface cursor-size 32
-    "
